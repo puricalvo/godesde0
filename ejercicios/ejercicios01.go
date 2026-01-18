300a94ce681cef65c03f1cbeb3725305b9b47f9b
puri calvo <puri_adrian@hotmail.com>
2026-01-18 06:15:53 +0100

    Siguiente Commit

---
+++ ejercicios/ejercicios01.go
@@ -0,0 +1,19 @@
+package ejercicios
+
+import (
+	"strconv"
+)
+
+func PrimerEjercicio(texto string) (int, string) {
+	numero, err := strconv.Atoi(texto)
+	if err != nil{
+		return 0, "Hubo un error en el mensaje" + err.Error()
+	}
+	if numero > 100 {
+		return numero, "Es mayor a 100"
+	}else {
+		return numero, "No es mayor a 100"
+	}
+
+	
+}
--- main.go
+++ main.go
@@ -3,11 +3,30 @@ package main
 import (
 	"fmt"
 
-	"github.com/puricalvo/godesde0/variables"
+	"github.com/puricalvo/godesde0/ejercicios"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(1588)
+	/* estado, texto := variables.ConviertoaTexto(1588)
 	fmt.Println(estado)
+	fmt.Println(texto) */
+	/* if os := runtime.GOOS; os=="linux" || os=="OS X."{
+		fmt.Println("Esto no es Windows, es ", os)
+	} else {
+		fmt.Println("Esto es Windows")
+	}
+
+	switch os:= runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+		
+	} */
+
+	numero, texto := ejercicios.PrimerEjercicio("500")
+	fmt.Println(numero)
 	fmt.Println(texto)
 }
